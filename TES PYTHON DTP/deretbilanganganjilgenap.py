47cb6acac6dccd83c24d1deb485d429e29c9842e
Nawfal <masnopal2008@gmail.com>
2025-10-22 13:01:20 +0700

    deretbilangan

--- TES PYTHON DTP/deretbilanganganjilgenap.py
+++ TES PYTHON DTP/deretbilanganganjilgenap.py
@@ -1,8 +1,8 @@
-angka_Awal = int(input("Masukkan angka awal: "))
-angka_Akhir = int(input("Masukkan angka akhir: "))
+baris_awal = int(input("masukkan bilangan awal : "))
+baris_akhir = int(input("masukkan bilangan akhir : "))
 
-for i in range(angka_Awal, angka_Akhir + 1):
-    if i % 2 == 0:
-        print(f"{i} adalah bilangan genap")
-    else:
-        print(f"{i} adalah bilangan ganjil")
+for i in range (baris_awal, baris_akhir+1):
+     if i % 2 == 0 :
+         print (i, "ini adalah bilangan genap")
+     else:
+         print (i, "ini adalah bilangan ganjil")
--- TES PYTHON DTP/deretbilanganprima.py
+++ TES PYTHON DTP/deretbilanganprima.py
@@ -1,6 +1,20 @@
-bilangan_awal = int(input("Masukkan angka awal: "))
-bilangan_akhir = int(input("Masukkan angka akhir: "))
-bilangan_prima = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, ]
-for i in range(bilangan_awal, bilangan_akhir + 1):
-    if i in bilangan_prima:
-        print(f"{i} adalah bilangan prima")
+print("kita mencari bilangan prima")
+
+def is_prime (num):
+
+    if num <= 1 :
+        return False
+
+    for i in range (2, int(num**0.5) + 1):
+        if num % i == 0:
+
+            return False
+
+        return True
+
+number = int(input("Masukkan bilangan anda : "))    
+
+if is_prime(number):
+    print(f"{number}Maka ini bilangan prima")
+else:
+    print(f"{number},kalo ini bukan bilangan prima")
